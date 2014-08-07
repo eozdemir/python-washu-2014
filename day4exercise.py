298f6e143a0c4a776360502f958430b51ce9c201
eozdemir <eozdemir@wustl.edu>
2014-08-07 16:20:19 -0500

    add day3 exercise
    Hanoi Towers

--- day4exercise.py
+++ day4exercise.py
@@ -33,5 +33,19 @@ def prime(num):
   return prime_list
   
 print prime(121)  
-  
+
+def hanoi(n, source, helper, target):
+  if n>0:
+    hanoi(n-1, source, target, helper)
+    if source:
+      target.append(source.pop())
+    hanoi(n-1, helper, source, target)
+
+source=[3,2,1]
+helper=[]
+target=[]
+hanoi(len(source), source, helper, target)
+
+print source, helper, target
+
   
