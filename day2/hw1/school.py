c83ccfb822c0f49e730ff8c12051d033251ebbbb
eozdemir <eozdemir@wustl.edu>
2014-08-07 18:50:15 -0500

    Add hw 1
    Grade-school

---
+++ day2/hw1/school.py
@@ -0,0 +1,29 @@
+class School():
+  def __init__(self, school):
+    self.school = school
+    self.db = {}
+	
+  def add(self, student, grade):
+    if type(grade) != int:
+  	  self.db = "Try to use integers for grade levels!"
+    else:
+      if grade in self.db:
+        self.db[grade].add(student)
+      else: 
+        self.db[grade] = {student}
+      
+  def sort(self):
+    sorted_students = {}
+    for i in sorted(self.db.keys()):
+      sorted_students[i] = sorted(self.db[i])
+      # since sorted forced them into list
+      sorted_students[i] = tuple(sorted_students[i] 
+    return sorted_students
+      
+  def grade(self, student_grade):
+  	if student_grade not in self.db:
+  	  return None
+  	else:
+  	  return self.db[student_grade]
+  	  
+    
