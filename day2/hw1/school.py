cd0c5d321386a7b6ef162e4edf7146191cdcfbe3
eozdemir <eozdemir@wustl.edu>
2014-08-07 19:15:53 -0500

    hw1 final
    Add comments to hw1

--- day2/hw1/school.py
+++ day2/hw1/school.py
@@ -1,29 +1,28 @@
 class School():
-  def __init__(self, school):
-    self.school = school
-    self.db = {}
+  def __init__(self, school):        # initialize the class
+    self.school = school 	         # only argument to generate an incidence of School class is school name
+    self.db = {}			         # create an empty dictionary to add students and grades
 	
-  def add(self, student, grade):
-    if type(grade) != int:
-  	  self.db = "Try to use integers for grade levels!"
-    else:
-      if grade in self.db:
-        self.db[grade].add(student)
-      else: 
-        self.db[grade] = {student}
+  def add(self, student, grade):     # function to add students and grades to dictionary
+    if type(grade) != int:           # in case user might enter invalid grade type
+      self.db = "Try to use integers for grade levels!" # also added a test for that
+    else:						     # function works with a proper grade type
+      if grade in self.db:		     # if the grade already exists in dictionary
+        self.db[grade].add(student)  # add the student name using given key for grade level
+      else: 						 # if the grade is not there 
+        self.db[grade] = {student}   # add a new key and assign a name for corresponding value 
       
-  def sort(self):
-    sorted_students = {}
-    for i in self.db.keys():
-      sorted_students[i] = sorted(self.db[i])
-      # since sorted forced them into list
-      sorted_students[i] = tuple(sorted_students[i])
-    return sorted_students
+  def sort(self):					 # function to sort the students in a grade
+    sorted_students = {}			 # a new/empty dictionary to add sorted students
+    for i in self.db.keys():		 # loop goes through the keys of dic.
+      sorted_students[i] = sorted(self.db[i]) # assigns sorted names into grade keys 
+      sorted_students[i] = tuple(sorted_students[i]) # since 'sorted' forced them into list
+    return sorted_students           # returns the new dictionary with sorted students
       
-  def grade(self, student_grade):
-  	if student_grade not in self.db:
-  	  return None
-  	else:
-  	  return self.db[student_grade]
+  def grade(self, student_grade):    # function to get students in a grade
+  	if student_grade not in self.db: # if no such grade exists 
+  	  return None					 # returns None according to test
+  	else:							 # if such grade exists
+  	  return self.db[student_grade]  # uses the grade key to reach out to student names
   	  
     
